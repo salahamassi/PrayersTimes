f67eebc8ccd0f149ba679f6e64e29c698b255fd5
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-21 02:34:13 +0300

    Replace magic number with variable giving extra context

--- PrayersTimes/PrayersTimesItemMapper.swift
+++ PrayersTimes/PrayersTimesItemMapper.swift
@@ -19,15 +19,16 @@ class PrayersTimesItemMapper {
             data.map(\.prayerTime)
         }
     }
+    
+    static var OK_200: Int { return 200 }
 
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [PrayersTimes] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemotePrayersTimesLoader.Error.invalidData
         }
 
         let root = try JSONDecoder().decode(Root.self, from: data)
         return root.prayersTimes
     }
-
 }
 
