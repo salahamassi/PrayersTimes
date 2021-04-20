fef6f7f8a8e9cfaf586b1bad5d741f985a2996e8
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-21 00:20:09 +0300

    Refactor test to remove duplication

--- PrayersTimesTests/PrayersTimesLoaderTests.swift
+++ PrayersTimesTests/PrayersTimesLoaderTests.swift
@@ -12,47 +12,52 @@ import XCTest
 class PrayersTimesLoader {
     
     let client: HTTPClient
-    
-    init(client: HTTPClient) {
+    let url: URL
+
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
 
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
-
 
 protocol HTTPClient {
     func get(from url: URL)
 }
 
 
-class HTTPClientSpy: HTTPClient {
-    func get(from url: URL) {
-        requestedURL = url
-    }
-    
-    var requestedURL: URL?
-}
-
-
 class PrayersTimesLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClientSpy()
-        _ = PrayersTimesLoader(client: client)
+        let (_, client) = makeSUT()
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestDataFromURL() {
-        let client = HTTPClientSpy()
-        let sut = PrayersTimesLoader(client: client)
+        let (sut, client) = makeSUT()
 
         sut.load()
 
         XCTAssertNotNil(client.requestedURL)
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: PrayersTimesLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = PrayersTimesLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    class HTTPClientSpy: HTTPClient {
+        func get(from url: URL) {
+            requestedURL = url
+        }
+        
+        var requestedURL: URL?
     }
 }
