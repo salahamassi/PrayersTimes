924490d49901dabbc51a0d8892fe1d3028d775f8
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-20 23:42:16 +0300

    Initial Commit

---
+++ PrayersTimes/PrayersTimes.h
@@ -0,0 +1,18 @@
+//
+//  PrayersTimes.h
+//  PrayersTimes
+//
+//  Created by Salah Amassi on 20/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrayersTimes.
+FOUNDATION_EXPORT double PrayersTimesVersionNumber;
+
+//! Project version string for PrayersTimes.
+FOUNDATION_EXPORT const unsigned char PrayersTimesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrayersTimes/PublicHeader.h>
+
+
