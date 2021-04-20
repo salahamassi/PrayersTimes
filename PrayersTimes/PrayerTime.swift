55fc2cc38dcf15392d085778bcb1768cbec51190
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-20 23:56:27 +0300

    Create the PrayerTime

---
+++ PrayersTimes/PrayerTime.swift
@@ -0,0 +1,24 @@
+//
+//  PrayerTime.swift
+//  PrayersTimes
+//
+//  Created by Salah Amassi on 20/04/2021.
+//
+
+import Foundation
+
+struct PrayerTime {
+    
+    let fajr: String
+    let sunrise: String
+    let dhuhr: String
+    let asr: String
+    let sunset: String
+    let maghrib: String
+    let isha: String
+    let imsak: String
+    let midnight: String
+    let data: Date
+}
+
+
