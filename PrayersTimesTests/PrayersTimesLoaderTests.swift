2265b09d5a4b25353e201c28968b0f781c0a7533
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-21 00:42:07 +0300

    Improve readability with helper method

--- PrayersTimesTests/PrayersTimesLoaderTests.swift
+++ PrayersTimesTests/PrayersTimesLoaderTests.swift
@@ -9,44 +9,44 @@ import XCTest
 import PrayersTimes
 
 class PrayersTimesLoaderTests: XCTestCase {
-
+    
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
-
+        
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
-
+    
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-
+        
         sut.load()
-
+        
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-
+        
         sut.load()
         sut.load()
-
+        
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
-
+    
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-
+        
         var capturedErrors = [RemotePrayersTimesLoader.Error]()
         sut.load { capturedErrors.append($0) }
         
         let clientError = NSError(domain: "Test", code: 0)
-        client.completions[0](clientError)
-
+        client.complete(with: clientError)
+        
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
-
+    
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemotePrayersTimesLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -57,10 +57,14 @@ class PrayersTimesLoaderTests: XCTestCase {
     class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
         var completions = [(Error) -> Void]()
-
+        
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 }
