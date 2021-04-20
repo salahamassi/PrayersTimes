91aa478d65c0bf62e2c9a3f25cd31606ea023981
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-21 00:11:38 +0300

    Request data from URL on `load`

--- PrayersTimesTests/PrayersTimesLoaderTests.swift
+++ PrayersTimesTests/PrayersTimesLoaderTests.swift
@@ -10,20 +10,37 @@ import Foundation
 import XCTest
 
 class PrayersTimesLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+    }
 
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+
+    private init() {}
+
     var requestedURL: URL?
 }
+
+
 
 class PrayersTimesLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = PrayersTimesLoader()
 
         XCTAssertNil(client.requestedURL)
     }
 
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = PrayersTimesLoader()
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL)
+    }
 }
