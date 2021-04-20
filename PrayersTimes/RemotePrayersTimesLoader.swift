6e598d4782258723eff6115d2accdf2f145a754c
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-21 00:32:31 +0300

    Delivers connectivity error on client error

--- PrayersTimes/RemotePrayersTimesLoader.swift
+++ PrayersTimes/RemotePrayersTimesLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(from url: URL, completion: @escaping (Error) -> Void)
 }
 
 public final class RemotePrayersTimesLoader {
@@ -16,14 +16,20 @@ public final class RemotePrayersTimesLoader {
     public let client: HTTPClient
     public let url: URL
 
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
 
-    public func load() {
-        client.get(from: url)
+    public func load(completion: @escaping (Error) -> Void = { _ in }) {
+        client.get(from: url) { error in
+            completion(.connectivity)
+        }
     }
 }
 
--- PrayersTimesTests/PrayersTimesLoaderTests.swift
+++ PrayersTimesTests/PrayersTimesLoaderTests.swift
@@ -35,6 +35,16 @@ class PrayersTimesLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
 
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+
+        var capturedError: RemotePrayersTimesLoader.Error?
+        sut.load { error in capturedError = error }
+
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+
     // MARK: - Helpers
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemotePrayersTimesLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
@@ -44,8 +54,12 @@ class PrayersTimesLoaderTests: XCTestCase {
     
     class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
 
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
