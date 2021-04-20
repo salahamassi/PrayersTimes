a3b85835c22a289dd93b84b15795ea9994abca5c
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-21 00:52:21 +0300

    Replace optional params with with success/failure cases

--- PrayersTimes/RemotePrayersTimesLoader.swift
+++ PrayersTimes/RemotePrayersTimesLoader.swift
@@ -7,30 +7,33 @@
 
 import Foundation
 
+
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+    typealias HTTPClientResult = Result<HTTPURLResponse, Error>
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemotePrayersTimesLoader {
     
     public let client: HTTPClient
     public let url: URL
-
+    
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
-
+    
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
-
+    
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { result in
+            switch result {
+            case .success:
                 completion(.invalidData)
-            } else {
+            case .failure:
                 completion(.connectivity)
             }
         }
--- PrayersTimesTests/PrayersTimesLoaderTests.swift
+++ PrayersTimesTests/PrayersTimesLoaderTests.swift
@@ -69,18 +69,18 @@ class PrayersTimesLoaderTests: XCTestCase {
     
     class HTTPClientSpy: HTTPClient {
         
-        private var messages = [(url: URL, completion:  (Error?, HTTPURLResponse?) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
 
         var requestedURLs: [URL] {
             messages.map(\.url)
         }
 
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         
         func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -89,8 +89,8 @@ class PrayersTimesLoaderTests: XCTestCase {
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            messages[index].completion(.success(response))
         }
     }
 }
