720133dce53f0a5a514ba41f7752a515c7784bbe
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-21 00:44:09 +0300

    Remove default value for completion block (moved it to the test)

--- PrayersTimesTests/PrayersTimesLoaderTests.swift
+++ PrayersTimesTests/PrayersTimesLoaderTests.swift
@@ -20,8 +20,8 @@ class PrayersTimesLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
-        
+        sut.load { _ in }
+
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
@@ -29,9 +29,9 @@ class PrayersTimesLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
-        sut.load()
-        
+        sut.load { _ in }
+        sut.load { _ in }
+
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
