49da3f504edeb3fb937dd93ff1c82513e5cfba46
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-21 06:40:02 +0300

    Add end-to-end API tests in a separate test target

---
+++ PrayersTimesApiEntToEndTests/PrayersTimesApiEntToEndTests.swift
@@ -0,0 +1,90 @@
+//
+//  PrayersTimesApiEntToEndTests.swift
+//  PrayersTimesApiEntToEndTests
+//
+//  Created by Salah Amassi on 21/04/2021.
+//
+
+import XCTest
+import PrayersTimes
+
+class PrayersTimesApiEntToEndTests: XCTestCase {
+    
+    func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
+        let testServerURL = URL(string: "http://api.aladhan.com/v1/calendar?latitude=31.524019&longitude=34.445422&method=5&month=04&year=1437")!
+        let client = URLSessionHTTPClient()
+        let loader = RemotePrayersTimesLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: RemotePrayersTimesLoader.Result?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        switch receivedResult {
+        case let .success(items)?:
+            XCTAssertEqual(items.count, 30, "Expected 30 items.")
+            XCTAssertEqual(items[0], expectedItem(at: 0))
+        case let .failure(error)?:
+            XCTFail("Expected successful feed result, got \(error) instead")
+            
+        default:
+            XCTFail("Expected successful feed result, got no result instead")
+        }
+    }
+    private func expectedItem(at index: Int) -> PrayersTimes {
+        return PrayersTimes(fajr: fajr(at: 0),
+                            sunrise: sunrise(at: 0),
+                            dhuhr: dhuhr(at: 0),
+                            asr: asr(at: 0),
+                            sunset: sunset(at: 0),
+                            maghrib: maghrib(at: 0),
+                            isha: isha(at: 0),
+                            imsak: imsak(at: 0),
+                            midnight: midnight(at: 0),
+                            date: date(at: 0))
+    }
+    
+    private func fajr(at index: Int) -> String {
+        ["04:20 (LMT)"][index]
+    }
+    
+    private func sunrise(at index: Int) -> String {
+        ["05:49 (LMT)"][index]
+    }
+    
+    private func dhuhr(at index: Int) -> String {
+        ["12:04 (LMT)"][index]
+    }
+    
+    private func asr(at index: Int) -> String {
+        ["15:36 (LMT)"][index]
+    }
+    
+    private func sunset(at index: Int) -> String {
+        ["18:19 (LMT)"][index]
+    }
+    
+    private func maghrib(at index: Int) -> String {
+        ["18:19 (LMT)"][index]
+    }
+    
+    private func isha(at index: Int) -> String {
+        ["19:39 (LMT)"][index]
+    }
+    
+    private func imsak(at index: Int) -> String {
+        ["04:10 (LMT)"][index]
+    }
+    
+    private func midnight(at index: Int) -> String {
+        ["00:04 (LMT)"][index]
+    }
+    
+    private func date(at index: Int) -> Date {
+        [Date(timeIntervalSince1970: -16812033411)][index]
+    }
+}
