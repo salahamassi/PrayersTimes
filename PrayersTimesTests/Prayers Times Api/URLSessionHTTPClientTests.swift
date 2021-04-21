5034bbfbd97b2025ac4993b9bb65afc8b46979c6
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-21 05:12:59 +0300

    Move the URLSessionHTTPClient (the system under test, or "SUT") creaion to a factory method to protect our tests from breaking changes

--- PrayersTimesTests/Prayers Times Api/URLSessionHTTPClientTests.swift
+++ PrayersTimesTests/Prayers Times Api/URLSessionHTTPClientTests.swift
@@ -48,7 +48,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
 
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
 
         wait(for: [exp], timeout: 1.0)
     }
@@ -57,12 +57,10 @@ class URLSessionHTTPClientTests: XCTestCase {
         let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
-
-        let sut = URLSessionHTTPClient()
         
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -78,6 +76,9 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
 
     // MARK: - Helpers
+    private func makeSUT() -> URLSessionHTTPClient {
+        URLSessionHTTPClient()
+    }
 
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
