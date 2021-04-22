6466cda47bbf4d05cb2f28709b11e8435bbc1087
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-22 05:47:31 +0300

    Move `LocalPrayersTimes` to its own file

---
+++ PrayersTimes/Prayers Times Cache/LocalPrayersTimes.swift
@@ -0,0 +1,37 @@
+//
+//  LocalPrayersTimes.swift
+//  PrayersTimes
+//
+//  Created by Salah Amassi on 22/04/2021.
+//
+
+import Foundation
+
+public struct LocalPrayersTimes: Equatable {
+    
+    public let fajr: String
+    public let sunrise: String
+    public let dhuhr: String
+    public let asr: String
+    public let sunset: String
+    public let maghrib: String
+    public let isha: String
+    public let imsak: String
+    public let midnight: String
+    public let date: Date
+    
+    public init(fajr: String, sunrise: String, dhuhr: String, asr: String, sunset: String, maghrib: String, isha: String, imsak: String, midnight: String, date: Date) {
+        self.fajr = fajr
+        self.sunrise = sunrise
+        self.dhuhr = dhuhr
+        self.asr = asr
+        self.sunset = sunset
+        self.maghrib = maghrib
+        self.isha = isha
+        self.imsak = imsak
+        self.midnight = midnight
+        self.date = date
+    }
+}
+
+
