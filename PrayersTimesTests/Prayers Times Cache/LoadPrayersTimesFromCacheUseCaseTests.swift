c574d0d7be384c0026c5280751ccdec3afa3ef45
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-22 06:08:39 +0300

    Extract duplicate test code into a shared helper method

--- PrayersTimesTests/Prayers Times Cache/LoadPrayersTimesFromCacheUseCaseTests.swift
+++ PrayersTimesTests/Prayers Times Cache/LoadPrayersTimesFromCacheUseCaseTests.swift
@@ -27,46 +27,41 @@ class LoadPrayersTimesFromCacheUseCaseTests: XCTestCase {
     func test_load_failsOnRetrievalError() {
         let (sut, store) = makeSUT()
         let retrievalError = anyNSError()
-        let exp = expectation(description: "Wait for load completion")
-
-        var receivedError: Error?
-        sut.load { result in
-            switch result {
-            case let .failure(error):
-                receivedError = error
-            default:
-                XCTFail("Expected failure, got \(result) instead")
-            }
-            exp.fulfill()
+        
+        expect(sut, toCompleteWith: .failure(retrievalError)) {
+            store.completeRetrieval(with: retrievalError)
         }
-
-        store.completeRetrieval(with: retrievalError)
-        wait(for: [exp], timeout: 1.0)
-
-        XCTAssertEqual((receivedError as NSError?)?.code, retrievalError.code)
-        XCTAssertEqual((receivedError as NSError?)?.domain, retrievalError.domain)
     }
     
     func test_load_deliversNoPrayersTimesOnEmptyCache() {
         let (sut, store) = makeSUT()
+        
+        expect(sut, toCompleteWith: .success([])) {
+            store.completeRetrievalWithEmptyCache()
+        }
+    }
+    
+    private func expect(_ sut: LocalPrayersTimesLoader, toCompleteWith expectedResult: LocalPrayersTimesLoader.LoadResult, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         let exp = expectation(description: "Wait for load completion")
 
-        var receivedPrayersTimes: [PrayersTimes]?
-        sut.load { result in
-            switch result {
-            case let .success(prayersTimes):
-                receivedPrayersTimes = prayersTimes
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedPrayersTimes), .success(expectedPrayersTimes)):
+                XCTAssertEqual(receivedPrayersTimes, expectedPrayersTimes, file: file, line: line)
+
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError.code, expectedError.code, file: file, line: line)
+                XCTAssertEqual(receivedError.domain, expectedError.domain, file: file, line: line)
             default:
-                XCTFail("Expected success, got \(result) instead")
+                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
             }
+            
             exp.fulfill()
         }
-
-        store.completeRetrievalWithEmptyCache()
+        action()
         wait(for: [exp], timeout: 1.0)
-
-        XCTAssertEqual(receivedPrayersTimes, [])
     }
+    
     // MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalPrayersTimesLoader, store: PrayersTimesStoreSpy) {
         let store = PrayersTimesStoreSpy()
