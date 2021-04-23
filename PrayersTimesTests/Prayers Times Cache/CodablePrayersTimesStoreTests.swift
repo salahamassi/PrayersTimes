1ba084d2af835a9fdf6bb266ce226545ae2f71d3
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-23 06:38:57 +0300

    Add helper methods to provide documentation (context), and to clarify test `setUp` and `tearDown` intent regarding side-effects

--- PrayersTimesTests/Prayers Times Cache/CodablePrayersTimesStoreTests.swift
+++ PrayersTimesTests/Prayers Times Cache/CodablePrayersTimesStoreTests.swift
@@ -87,13 +87,15 @@ class CodablePrayersTimesStoreTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        setupEmptyStoreState()
     }
     
     override func tearDown() {
         super.tearDown()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
-    }
+        undoStoreSideEffects()
+        Add helper methods to provide documentation (context), and to clarify…
+
+        … test `setUp` and `tearDown` intent regarding side-effects    }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = makeSUT()
@@ -168,6 +170,18 @@ class CodablePrayersTimesStoreTests: XCTestCase {
         return sut
     }
     
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+
     private func testSpecificStoreURL() -> URL {
         FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
     }
