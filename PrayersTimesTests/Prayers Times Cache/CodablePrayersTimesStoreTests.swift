289735c8b73b5061b8d3a18bfef4a530ba743238
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-23 06:40:59 +0300

    Add helper methods to provide documentation (context), and to clarify test `setUp` and `tearDown` intent regarding side-effects

--- PrayersTimesTests/Prayers Times Cache/CodablePrayersTimesStoreTests.swift
+++ PrayersTimesTests/Prayers Times Cache/CodablePrayersTimesStoreTests.swift
@@ -87,12 +87,12 @@ class CodablePrayersTimesStoreTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        setupEmptyStoreState()
     }
     
     override func tearDown() {
         super.tearDown()
-        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+        undoStoreSideEffects()
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -166,6 +166,18 @@ class CodablePrayersTimesStoreTests: XCTestCase {
         let sut = CodablePrayersTimesStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     private func testSpecificStoreURL() -> URL {
