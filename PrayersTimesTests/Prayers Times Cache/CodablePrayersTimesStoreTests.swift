7693278edef2504915c40715b2137bf320eff0e8
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-23 13:52:28 +0300

    Make the storeURL explicit within the test to facilitate debugging if this test ever fails (all relevant details within a test method should be clearly visible).

--- PrayersTimesTests/Prayers Times Cache/CodablePrayersTimesStoreTests.swift
+++ PrayersTimesTests/Prayers Times Cache/CodablePrayersTimesStoreTests.swift
@@ -131,15 +131,16 @@ class CodablePrayersTimesStoreTests: XCTestCase {
     }
     
     func test_retrieve_deliversFailureOnRetrievalError() {
-        let sut = makeSUT()
-        
-        try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+        let storeURL = testSpecificStoreURL()
+        let sut = makeSUT(storeURL: storeURL)
+
+        try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
         
         expect(sut, toRetrieve: .failure(anyNSError()))
     }
     
     // - MARK: Helpers
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodablePrayersTimesStore {
+    private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodablePrayersTimesStore {
         let sut = CodablePrayersTimesStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
