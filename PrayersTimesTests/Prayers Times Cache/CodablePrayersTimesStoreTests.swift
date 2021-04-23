ba0402daabd724d11027884e301b1587eec6f6ba
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-23 14:53:50 +0300

    Break down `CodablePrayersTimesStore` tests to guarantee there's only one assertion per test. The goal is to clarify the behavior under test in small units, so we can extract the behavior tests into reusable specs.

--- PrayersTimesTests/Prayers Times Cache/CodablePrayersTimesStoreTests.swift
+++ PrayersTimesTests/Prayers Times Cache/CodablePrayersTimesStoreTests.swift
@@ -92,6 +92,16 @@ class CodablePrayersTimesStoreTests: XCTestCase {
         let insertionError = insert((prayersTimes, timestamp), to: sut)
         
         XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+    }
+    
+    func test_insert_hasNoSideEffectsOnInsertionError() {
+        let invalidStoreURL = URL(string: "invalid://store-url")!
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        let prayersTimes = uniqueItems().local
+        let timestamp = Date()
+        
+        insert((prayersTimes, timestamp), to: sut)
+        
         expect(sut, toRetrieve: .empty)
     }
     
@@ -119,40 +129,48 @@ class CodablePrayersTimesStoreTests: XCTestCase {
     func test_delete_deliversErrorOnDeletionError() {
         let noDeletePermissionURL = cachesDirectory()
         let sut = makeSUT(storeURL: noDeletePermissionURL)
-
+        
         let deletionError = deleteCache(from: sut)
-
+        
         XCTAssertNotNil(deletionError, "Expected cache deletion to fail")
+    }
+    
+    func test_delete_hasNoSideEffectsOnDeletionError() {
+        let noDeletePermissionURL = cachesDirectory()
+        let sut = makeSUT(storeURL: noDeletePermissionURL)
+        
+        deleteCache(from: sut)
+        
         expect(sut, toRetrieve: .empty)
     }
     
     func test_storeSideEffects_runSerially() {
         let sut = makeSUT()
         var completedOperationsInOrder = [XCTestExpectation]()
-
+        
         let op1 = expectation(description: "Operation 1")
         sut.insert(uniqueItems().local, timestamp: Date()) { _ in
             completedOperationsInOrder.append(op1)
             op1.fulfill()
         }
-
+        
         let op2 = expectation(description: "Operation 2")
         sut.deleteCachedPrayersTimes { _ in
             completedOperationsInOrder.append(op2)
             op2.fulfill()
         }
-
+        
         let op3 = expectation(description: "Operation 3")
         sut.insert(uniqueItems().local, timestamp: Date()) { _ in
             completedOperationsInOrder.append(op3)
             op3.fulfill()
         }
-
+        
         waitForExpectations(timeout: 5.0)
-
+        
         XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order")
     }
-
+    
     // - MARK: Helpers
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #filePath, line: UInt = #line) -> CodablePrayersTimesStore {
         let sut = CodablePrayersTimesStore(storeURL: storeURL ?? testSpecificStoreURL())
@@ -172,6 +190,7 @@ class CodablePrayersTimesStoreTests: XCTestCase {
         return insertionError
     }
     
+    @discardableResult
     private func deleteCache(from sut: CodablePrayersTimesStore) -> Error? {
         let exp = expectation(description: "Wait for cache deletion")
         var deletionError: Error?
