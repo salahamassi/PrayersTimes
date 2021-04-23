e33d1884da6f9d17734e60234566b828fedffc0c
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-23 04:24:32 +0300

    LocalPrayersTimesLoader does not message store upon creation (before validating the cached prayers times)

---
+++ PrayersTimesTests/Prayers Times Cache/ValidatePrayersTimesCacheUseCaseTests.swift
@@ -0,0 +1,31 @@
+//
+//  ValidatePrayersTimesCacheUseCaseTests.swift
+//  PrayersTimesTests
+//
+//  Created by Salah Amassi on 23/04/2021.
+//
+
+import XCTest
+import PrayersTimes
+
+class ValidatePrayersTimesCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalPrayersTimesLoader, store: PrayersTimesStoreSpy) {
+        let store = PrayersTimesStoreSpy()
+        let sut = LocalPrayersTimesLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private func anyNSError() -> NSError {
+        .init(domain: "any error", code: 0)
+    }
+}
