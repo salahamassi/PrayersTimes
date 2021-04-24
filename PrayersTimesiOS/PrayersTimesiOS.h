7e38f33d1ebf6ef34fd852d59d833bf8c720a0db
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-24 04:01:48 +0300

    Add `PrayersTimesiOS` framework

---
+++ PrayersTimesiOS/PrayersTimesiOS.h
@@ -0,0 +1,18 @@
+//
+//  PrayersTimesiOS.h
+//  PrayersTimesiOS
+//
+//  Created by Salah Amassi on 24/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrayersTimesiOS.
+FOUNDATION_EXPORT double PrayersTimesiOSVersionNumber;
+
+//! Project version string for PrayersTimesiOS.
+FOUNDATION_EXPORT const unsigned char PrayersTimesiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrayersTimesiOS/PublicHeader.h>
+
+
