999632a1bddba243729c25569d01c8e7a1f63cd3
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-25 01:44:31 +0300

    mapping PrayersTimes throw error on invalid prayer time

---
+++ PrayersTimesTests/PrayersTimesMapperTests.swift
@@ -0,0 +1,36 @@
+//
+//  PrayersTimesMapperTests.swift
+//  PrayersTimesTests
+//
+//  Created by Salah Amassi on 25/04/2021.
+//
+
+import XCTest
+
+class PrayersTimesMapper {
+    
+    enum Error: Swift.Error {
+        case invalidPrayerTime(String)
+    }
+    
+    static func map(_ prayersTimes: String ..., using date: Date) throws -> [Date] {
+        throw Error.invalidPrayerTime(prayersTimes.first!)
+    }
+}
+
+class PrayersTimesMapperTests: XCTestCase {
+
+    func test_map_throwErrorOnInvalidDate() {
+        let sut = PrayersTimesMapper.self
+        
+        var receivedError: Error?
+        do {
+            _ = try sut.map("invalid prayer time", using: Date())
+        } catch {
+            receivedError = error
+        }
+        
+        XCTAssertNotNil(receivedError)
+    }
+
+}
