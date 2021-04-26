5b94fbe58d997dafcceb6c3d88f66805cac3a9ba
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-27 01:50:17 +0300

    get next prayer date for current date

--- PrayersTimesTests/NextPrayerUseCaseTests.swift
+++ PrayersTimesTests/NextPrayerUseCaseTests.swift
@@ -22,6 +22,27 @@ class NextPrayerUseCase {
         let calendar = Calendar.init(identifier: .gregorian)
         return prayersTimes.first(where: { calendar.isDate($0.day, inSameDayAs: currentDate()) })
     }
+    
+    func getNextPrayerDateForCurrentDate() -> Date? {
+        guard let prayersTimes = getPrayersTimesForCurrentDate() else { return nil }
+        let date = currentDate()
+        let dates = [date,
+                     prayersTimes.prayers[.fajr],
+                     prayersTimes.prayers[.sunrise],
+                     prayersTimes.prayers[.dhuhr],
+                     prayersTimes.prayers[.asr],
+                     prayersTimes.prayers[.sunset],
+                     prayersTimes.prayers[.maghrib],
+                     prayersTimes.prayers[.isha],
+                     prayersTimes.prayers[.imsak],
+                     prayersTimes.prayers[.midnight]].sorted()
+        guard let firstIndex = dates.firstIndex(of: date) else { return nil }
+        if firstIndex < dates.count - 1 {
+            let index = firstIndex + 1
+            return dates[index]
+        }
+        return nil
+    }
 }
 
 class NextPrayerUseCaseTests: XCTestCase {
@@ -37,7 +58,21 @@ class NextPrayerUseCaseTests: XCTestCase {
         
         let sut = NextPrayerUseCase(prayersTimes: [yesterdayItem, todayItem, tomorrowItem], currentDate: Date.init)
         
-
+        
         XCTAssertEqual(sut.getPrayersTimesForCurrentDate(), todayItem)
+    }
+    
+    func test_getNextPrayerDateForCurrentDate() {
+        let currentDate = staticDate // Saturday, April 24, 2021 9:00:00 PM GMT
+        let yesterdayDate = currentDate.adding(day: -1)
+        let tomorrowDate = currentDate.adding(day: 1)
+        
+        let yesterdayItem = uniqueItem(using: yesterdayDate)
+        let todayItem = uniqueItem(using: currentDate)
+        let tomorrowItem = uniqueItem(using: tomorrowDate)
+        
+        let sut = NextPrayerUseCase(prayersTimes: [yesterdayItem, todayItem, tomorrowItem], currentDate: { currentDate })
+        let expectedPrayerDate = todayItem.prayers[.midnight] // Saturday, April 24, 2021 9:46:00 PM GMT
+        XCTAssertEqual(sut.getNextPrayerDateForCurrentDate(), expectedPrayerDate)
     }
 }
