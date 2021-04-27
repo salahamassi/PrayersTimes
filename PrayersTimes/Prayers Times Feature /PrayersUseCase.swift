18d586e8c122e289f1f720fe3766e5e66c9091dc
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-27 04:44:33 +0300

    extract PrayersUseCase to it's own file

---
+++ PrayersTimes/Prayers Times Feature /PrayersUseCase.swift
@@ -0,0 +1,38 @@
+//
+//  PrayersUseCase.swift
+//  PrayersTimes
+//
+//  Created by Salah Amassi on 27/04/2021.
+//
+
+import Foundation
+
+public class PrayersUseCase {
+    
+    public let currentDate: ()-> Date
+    public let prayersTimes: [PrayersTimes]
+    
+    public init(prayersTimes: [PrayersTimes], currentDate: @escaping () -> Date) {
+        self.prayersTimes = prayersTimes
+        self.currentDate = currentDate
+    }
+    
+    public func getPrayersTimes() -> PrayersTimes? {
+        let calendar = Calendar.init(identifier: .gregorian)
+        return prayersTimes.first(where: { calendar.isDate($0.day, inSameDayAs: currentDate()) })
+    }
+    
+    public func getNextPrayerDate() -> Prayers.Prayer? {
+        guard let prayersTimes = getPrayersTimes() else { return nil }
+        let prayers = [prayersTimes.prayers[.fajr],
+                       prayersTimes.prayers[.sunrise],
+                       prayersTimes.prayers[.dhuhr],
+                       prayersTimes.prayers[.asr],
+                       prayersTimes.prayers[.sunset],
+                       prayersTimes.prayers[.maghrib],
+                       prayersTimes.prayers[.isha],
+                       prayersTimes.prayers[.imsak],
+                       prayersTimes.prayers[.midnight]]
+        return prayers.first(where: { $0.date >  currentDate() && ($0.type != .sunset && $0.type != .sunrise && $0.type != .imsak && $0.type != .midnight) })
+    }
+}
--- PrayersTimesTests/Prayers Use Case/PrayersUseCaseTests.swift
+++ PrayersTimesTests/Prayers Use Case/PrayersUseCaseTests.swift
@@ -8,36 +8,6 @@
 import XCTest
 import PrayersTimes
 
-class PrayersUseCase {
-    
-    let currentDate: ()-> Date
-    let prayersTimes: [PrayersTimes]
-    
-    init(prayersTimes: [PrayersTimes], currentDate: @escaping () -> Date) {
-        self.prayersTimes = prayersTimes
-        self.currentDate = currentDate
-    }
-    
-    func getPrayersTimes() -> PrayersTimes? {
-        let calendar = Calendar.init(identifier: .gregorian)
-        return prayersTimes.first(where: { calendar.isDate($0.day, inSameDayAs: currentDate()) })
-    }
-    
-    func getNextPrayerDate() -> Prayers.Prayer? {
-        guard let prayersTimes = getPrayersTimes() else { return nil }
-        let prayers = [prayersTimes.prayers[.fajr],
-                     prayersTimes.prayers[.sunrise],
-                     prayersTimes.prayers[.dhuhr],
-                     prayersTimes.prayers[.asr],
-                     prayersTimes.prayers[.sunset],
-                     prayersTimes.prayers[.maghrib],
-                     prayersTimes.prayers[.isha],
-                     prayersTimes.prayers[.imsak],
-                     prayersTimes.prayers[.midnight]]
-        return prayers.first(where: { $0.date >  currentDate() && ($0.type != .sunset && $0.type != .sunrise && $0.type != .imsak && $0.type != .midnight) })
-    }
-}
-
 class PrayersUseCaseTests: XCTestCase {
     
     func test_getPrayersTimes() {
