33eb122fd4afee3a253ac5ab37c233b59f2c4545
Salah Nahed <salahamassi@users.noreply.github.com>
2021-04-27 05:07:18 +0300

    move some helper method to shared scope

--- PrayersTimesApiEntToEndTests/PrayersTimesApiEntToEndTests.swift
+++ PrayersTimesApiEntToEndTests/PrayersTimesApiEntToEndTests.swift
@@ -42,16 +42,16 @@ class PrayersTimesApiEntToEndTests: XCTestCase {
     }
 
     private func expectedItem(at index: Int) -> PrayersTimes {
-        .init(fajr: fajr(at: 0),
-                            sunrise: sunrise(at: 0),
-                            dhuhr: dhuhr(at: 0),
-                            asr: asr(at: 0),
-                            sunset: sunset(at: 0),
-                            maghrib: maghrib(at: 0),
-                            isha: isha(at: 0),
-                            imsak: imsak(at: 0),
-                            midnight: midnight(at: 0),
-                            date: date(at: 0))
+        .init(prayers: (fajr: getDate(from: fajr(at: 0), using: date(at: 0)),
+                            sunrise: getDate(from: sunrise(at: 0), using: date(at: 0)),
+                            dhuhr: getDate(from: dhuhr(at: 0), using: date(at: 0)),
+                            asr: getDate(from: asr(at: 0), using: date(at: 0)),
+                            sunset: getDate(from: sunset(at: 0), using: date(at: 0)),
+                            maghrib: getDate(from: maghrib(at: 0), using: date(at: 0)),
+                            isha: getDate(from: isha(at: 0), using: date(at: 0)),
+                            imsak: getDate(from: imsak(at: 0), using: date(at: 0)),
+                            midnight: getDate(from: midnight(at: 0), using: date(at: 0))),
+                            for: date(at: 0))
     }
     
     private func fajr(at index: Int) -> String {
--- PrayersTimesTests/Prayers Times Cache/Helpers/PrayersTimesCacheTestHelpers.swift
+++ PrayersTimesTests/Prayers Times Cache/Helpers/PrayersTimesCacheTestHelpers.swift
@@ -6,59 +6,6 @@
 //
 
 import Foundation
-import PrayersTimes
-
-
-let staticDate = Date(timeIntervalSince1970: 1619298000)
-
-func uniqueItem(using date: Date = staticDate) -> PrayersTimes {
-    .init(prayers: (fajr: getDate(from: "05:01 (EEST)", using: date),
-                    sunrise: getDate(from: "06:30 (EEST)", using: date),
-                    dhuhr: getDate(from: "12:46 (EEST)", using: date),
-                    asr: getDate(from: "16:18 (EEST)", using: date),
-                    sunset: getDate(from: "19:02 (EEST)", using: date),
-                    maghrib: getDate(from: "19:02 (EEST)", using: date),
-                    isha: getDate(from: "20:22 (EEST)", using: date),
-                    imsak: getDate(from: "04:50 (EEST)", using: date),
-                    midnight: getDate(from: "00:46 (EEST)", using: date)),
-          for: date)
-}
-
-func uniqueItems() -> (models: [PrayersTimes], local: [LocalPrayersTimes]) {
-    let models = [uniqueItem(), uniqueItem()]
-    let local = models.map{ LocalPrayersTimes(prayers: (fajr: $0.prayers[.fajr].date,
-                                                        sunrise: $0.prayers[.sunrise].date,
-                                                        dhuhr: $0.prayers[.dhuhr].date,
-                                                        asr: $0.prayers[.asr].date,
-                                                        sunset: $0.prayers[.sunset].date,
-                                                        maghrib: $0.prayers[.maghrib].date,
-                                                        isha: $0.prayers[.isha].date,
-                                                        imsak: $0.prayers[.imsak].date,
-                                                        midnight: $0.prayers[.midnight].date),
-                                              for: $0.day) }
-    return (models, local)
-}
-
-func getDate(from string: String, using date: Date) -> Date {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "MM/dd/yyyy"
-    let fullDateString = dateFormatter.string(from: date)
-    
-    dateFormatter.dateFormat = "MM/dd/yyyy HH:mm:ss"
-    
-    var stringResultDate = ""
-    let splitResult = string.split(separator: " ")
-    
-    if let first = splitResult.first, let last = splitResult.last {
-        dateFormatter.timeZone = TimeZone(abbreviation: String(last))
-        stringResultDate.append(fullDateString)
-        stringResultDate.append(" \(first):00")
-        if let resultDate = dateFormatter.date(from: stringResultDate) {
-            return resultDate
-        }
-    }
-    fatalError("wrong string format \(string)")
-}
 
 extension Date {
     
